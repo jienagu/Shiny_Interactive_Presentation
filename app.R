38cd0522e450279b0ec5582dcc9b56cdfc7e7785
Jiena <jienagu9085@gmail.com>
2019-04-22 14:01:59 -0500

    Update app.R

--- app.R
+++ app.R
@@ -1,85 +1,96 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
-library(dplyr)
-library(purrr)
-library(rlang)
-library(stringr)
 # devtools::install_github("jienagu/noteMD")
 library(noteMD)
 library(DT)
 library(r2d3)
 library(webshot)
 library(htmlwidgets)
-library(memor)
-
- webshot::install_phantomjs()
-# load data
+library(leaflet)
+library(rlang)
+library(dplyr)
+# webshot::install_phantomjs()
+## Updated
 temp_flight2=readRDS("temp_flight2.rds")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   
   # Application title
-  titlePanel("D3 Interactive Bar Chart"),
+  titlePanel("Create Interactive Presentation from Shiny"),
   
   # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      selectInput(
-        inputId = "airlines",
-        label = "Airlines:",
-        choices = c("ALL", unique(temp_flight2$name)),
-        size = 10,selectize = FALSE,
-        selected = "ALL"
-      ),
-      textInput("author", "Please write your name here:", "Your name"),
-     # actionButton("remove", "Remove data table(s)!"),br(),br(),br(),
-      downloadButton('describe_download',"Download Report",class="butt" ),br(),
-      tags$head(tags$style(".butt{background-color:#230682;} .butt{color: #e6ebef;}")),
-      radioButtons('format', 'Document format', c('PDF', 'Word'),
-                   inline = TRUE),br(),
-     tags$head(tags$style(".button{background-color:#032863;} .button{color: #e6ebef;}")),
-   #  downloadButton('presentation_download',"Download Presentation",class="button" ),br(),br(),
-     downloadButton('presentation_download2',"Download Interactive Presentation",class="button" ),br()
-    ),
-    
+  textInput("author", "Please write down your name here:", "古杰娜"),
+  textInput("presentation_title", "Title of your presentation:", "这里写标题"),
+  tags$head(tags$style(".button{background-color:#032863;} .button{color: #e6ebef;}")),
+  downloadButton('presentation_download2',"Download Interactive Presentation",class="button" ),br(),br(),
+  ### CSS color to change the color of interface
+  tags$style(HTML("
+    .tabbable > .nav > li > a[data-value='page1'] {background-color: #F3B7C3;  color:black}
+    .tabbable > .nav > li > a[data-value='page2'] {background-color: #acd2ee;   color:black}
+    .tabbable > .nav > li > a[data-value='page3'] {background-color: #eedeac;  color:black}
+    .shiny-input-container { color:#a27839}
+    .container-fluid {color:navy; background-color: #f4dee7}
+  ")),
     # Show a plot of the generated distribution
-    mainPanel(
+  fluidPage(
       tabsetPanel(
         id = "tabs",
         tabPanel(
-          title = "Analytics Dashboard",
-          value = "page1",
-          fluidRow(
-            column(
-              width = 8,
-              d3Output("airbar")
-            )
-          ),
+          title = "Slide 1",
+          value = "page1",br(),
+          textInput("slide1_title", "Slide 1 Title:", "标题1"),br(),
           fluidRow(
             column(12,
                    helpText("Note: Any comments made in the box will be printed if you download the summary report.") ),
             column(12,
                    tags$textarea(
-                     "Please using any **markdown** syntax!",
+"重要的事情**说三遍**：*大碗宽面*
+
+* 第一遍: 用 `UTF-8`
+* 第二遍: 用 `UTF-8`
+* 第三遍: 吼吼吼~用 `UTF-8` ",
                      id    = 'markdowninput',
                      rows  = 3,
                      style = 'width:100%;')) ),
           helpText("Preview:"),
           htmlOutput('htmlmarkdown'),br(),br()
+        ),
+        tabPanel(
+          title = "Slide 2",
+          value = "page2",br(),
+          textInput("slide2_title", "Slide 2 Title:", "标题2"),br(),
+          fluidRow(
+            column(
+              width = 4,
+              selectInput(
+                inputId = "airlines",
+                label = "Airlines:",
+                choices = c("ALL", unique(temp_flight2$name)),
+                size = 16,selectize = FALSE,
+                selected = "ALL"
+              ) ),
+            column(
+              width = 8,
+              d3Output("airbar")
+            )
+          )
+        ),
+        tabPanel(
+          title = "Slide 3",
+          value = "page3",br(),
+          textInput("slide3_title", "Slide 3 Title:", "标题3"),br(),
+          fluidRow(
+            column(
+              width = 12,
+              leafletOutput("mymap")
+              )
+          )
         )
-      )
     )
   )
-)
+  
+)  
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
@@ -88,11 +99,19 @@ server <- function(input, output, session) {
     note_in_html(input$markdowninput)
   })
   
-  tab_list <- NULL
   
   sel_flights <- reactive({
     if (input$airlines != "ALL") temp_flight2 <- filter(temp_flight2, name == input$airlines)
     temp_flight2
+  })
+  
+  mymap_reactive=reactive({
+    leaflet() %>%
+      addTiles() %>%  # Add default OpenStreetMap map tiles
+      addMarkers(lng=-96.571671, lat=39.183609, popup="Where I am!")
+  })
+  output$mymap <- renderLeaflet({
+    mymap_reactive()
   })
   
   bar_graphD3=reactive({
@@ -126,96 +145,6 @@ server <- function(input, output, session) {
     } 
    }, ignoreInit = TRUE )
  
-  
-  output$describe_download = downloadHandler(
-    filename<- function(){
-      paste("Summary",Sys.Date(),switch(
-        input$format, PDF = '.pdf', Word = '.docx'
-      ),sep = "")
-    },
-    
-    content = function(file) {
-      if (input$format=="PDF"){
-        #### Progressing indicator
-        withProgress(message = 'Download in progress',
-                     detail = 'This may take a while...', value = 0, {
-                       for (i in 1:15) {
-                         incProgress(1/15)
-                         Sys.sleep(0.01)
-                       }
-                       
-                       ## End of progression
-                       src <- normalizePath('summary_report.Rmd')
-                       
-                       # temporarily switch to the temp dir, in case you do not have write
-                       # permission to the current working directory
-                       owd <- setwd(tempdir())
-                       on.exit(setwd(owd))
-                       file.copy(src, 'summary_report.Rmd', overwrite = TRUE)
-                       
-                       library(rmarkdown)
-                       out <- render('summary_report.Rmd', pdf_document())
-                       file.rename(out, file)
-                       
-                     })
-        ### below is the end of pdf content
-      }else{
-        withProgress(message = 'Download in progress',
-                     detail = 'This may take a while...', value = 0, {
-                       for (i in 1:15) {
-                         incProgress(1/15)
-                         Sys.sleep(0.01)
-                       }
-                       
-                       ## End of progression
-                       src <- normalizePath('summary_report_word.Rmd')
-                       
-                       # temporarily switch to the temp dir, in case you do not have write
-                       # permission to the current working directory
-                       owd <- setwd(tempdir())
-                       on.exit(setwd(owd))
-                       file.copy(src, 'summary_report_word.Rmd', overwrite = TRUE)
-                       
-                       library(rmarkdown)
-                       out <- render('summary_report_word.Rmd', word_document())
-                       file.rename(out, file)
-                     })
-      }
-      
-    })
-  
-  # output$presentation_download = downloadHandler(
-  #   filename<- function(){
-  #     paste("Presentation",Sys.Date(),".pdf",sep = "")
-  #   },
-  #   
-  #   content = function(file) {
-  #       #### Progressing indicator
-  #       withProgress(message = 'Download in progress',
-  #                    detail = 'This may take a while...', value = 0, {
-  #                      for (i in 1:15) {
-  #                        incProgress(1/15)
-  #                        Sys.sleep(0.01)
-  #                      }
-  #                      
-  #                      ## End of progression
-  #                      src <- normalizePath('presentation_summary.Rmd')
-  #                      
-  #                      # temporarily switch to the temp dir, in case you do not have write
-  #                      # permission to the current working directory
-  #                      owd <- setwd(tempdir())
-  #                      on.exit(setwd(owd))
-  #                      file.copy(src, 'presentation_summary.Rmd', overwrite = TRUE)
-  #                      
-  #                      library(rmarkdown)
-  #                      out <- render('presentation_summary.Rmd',  beamer_presentation())
-  #                      file.rename(out, file)
-  #                      
-  #                    })
-  # 
-  #     
-  #   },
-  #   contentType = 'application/pdf')
   
   output$presentation_download2 = downloadHandler(
     filename<- function(){
